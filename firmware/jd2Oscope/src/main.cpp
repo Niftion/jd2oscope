9372b57bd4856390470f574bde319a0bc5b9f736
StPelican42 <N.Fontenot_2022@outlook.com>
2025-03-06 05:49:23 -0800

    Prototype code with trigger, zoom, Amplitude, and pause
    Edited to have less garbage pseudocode. Easier to read now.

--- firmware/jd2Oscope/src/main.cpp
+++ firmware/jd2Oscope/src/main.cpp
@@ -28,38 +28,18 @@ float real_data[100];
 float trigger_data[200];
 volatile byte pause_state = LOW;
 volatile byte trigger_state = LOW;
-//#include "font_Arial.h"
-//#include <Adafruit_ILI9341.h>   // include Adafruit ILI9341 TFT library
-// #include <Adafruit_GFX.h>       // include Adafruit graphics library
-// #include <Adafruit_BusIO_Register.h>
+
 
 
 /*
-#include <implot.h>
-#include <implot_internal.h>
-#include <implot.cpp>
-#include <implot_items.cpp>
-#include "imgui.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx11.h"
-#include <math.h>
-#include <time.h>
-#include <driver/gpio.h>
+
 */
 
-//char receivedChar;
-//char receivedChars[32];
-//char endMarker = '.';
-//int j =0;
-//bool eor = false;
-//static void big_timer_callback(void* arg);
-//static void small_timer_callback(void* arg);
+
 //int ADC_val_converter(int example_input);
 //static double volts_to_bits(double volt_in);
 //static double bits_to_volts(double bit_in);
-//int button = 23; // io 0 -> leads to interrupt reset? trying io 23 now
-//bool Ch1 = 1;
-//bool Ch2 = 0;
+
 
 //Display Screen Functions
 
@@ -82,21 +62,10 @@ void pause_button();
 void trigger_set();
 void trigger_function();
 
-//Setup for Display screen
 
-// initialize ILI9341 TFT library
-//TFT_eSPI tft = TFT_eSPI();
-
-// put function declarations here:
 
 void setup() {
-  //pinMode(button,INPUT);
-  //pinMode(33, OUTPUT); // use IO21 as output
-  
-  // put your setup code here, to run once:
 
-  //Serial.println("ILI9341 Test!"); 
- 
   //tft.begin();
   // Note: you can now set the SPI speed to any value
   // the default value is 30Mhz, but most ILI9341 displays
@@ -106,40 +75,34 @@ void setup() {
     //tft.setTextColor(ILI9341_YELLOW);
     //tft.setTextSize(2);
     //tft.println("Waiting for Arduino Serial Monitor...");
-
-     Serial.begin(115200);
+  //********************************************************************
+  // Graph Setup
+  //********************************************************************
+  Serial.begin(115200);
   delay(5000);
   tft.begin();
   tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
-
   // Graph area is 200 pixels wide, 150 high, dark grey background
   gr.createGraph(290, 190, tft.color565(5, 5, 5));
-
   // x scale units is from 0 to 100, y scale units is -50 to 50
   gr.setGraphScale(0.0, 100.0, -50.0, 50.0);
-
   // X grid starts at 0 with lines every 10 x-scale units
   // Y grid starts at -50 with lines every 25 y-scale units
   // blue grid
   gr.setGraphGrid(0.0, 10.0, -50.0, 10.0, TFT_BLUE);
-
   // Draw empty graph, top left corner at 40,10 on TFT
   gr.drawGraph(20, 20);
-
   // Start a trace with using red and another with green
   tr1.startTrace(TFT_RED);
   tr2.startTrace(TFT_GREEN);
-
   // Add points on graph to trace 1 using graph scale factors
   tr1.addPoint(0.0, 0.0);
   tr1.addPoint(50.0, 0.0);
-
   // Add points on graph to trace 2 using graph scale factors
   // Points are off graph so the plotted line is clipped to graph area
   tr2.addPoint(0.0, -50.0);
   tr2.addPoint(50.0, 50.0);
-
   // Get x,y pixel coordinates of any scaled point on graph
   // and ring that point.
   tft.drawCircle(gr.getPointX(50.0), gr.getPointY(0.0), 5, TFT_MAGENTA);
@@ -183,46 +146,39 @@ void setup() {
   tr1.startTrace(TFT_WHITE);
   tr2.startTrace(TFT_YELLOW);
   
- 
-  //Serial.println("Enter t to test timer, c to test ADC conversions, or g to test graph");
 
-  //++++++++++++ESPTimer start code
-
-  //++++++++++++Interrupts for Pausing and Channel Select
-  
+  //++++++++++++Interrupts for Pausing, Trigger, and Channel Select
+  //int pause_pin
+  //int trigger_pin
+  //int Ch1_pin
+  //int Ch2_pin
   pinMode(26, INPUT_PULLUP);// Using GPIO 6 for button testing
 
   attachInterrupt(digitalPinToInterrupt(26), trigger_set, FALLING); //was pause_button, now trigger_state
-
+  //attachInterrupt(digitalPinToInterrupt(pause_pin), pause_button, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(trigger_pin), trigger_set, FALLING);
 }
 
 void loop() {
-  //while(pause_state){
-  //  gr.drawGraph(20, 20);
-    //for (int i =0; i<100; i++){
-      //tr2.addPoint(i, display_data[i]);
-      //tr1.addPoint(i, display_data[i]);
-    //}
-  //}
+  
   while(!pause_state){
     tft.fillRect(150, 5, 100, 10, TFT_BLACK);
     tft.setCursor(150, 5);
     tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
-  if(!trigger_state){
-    tft.println("Trigger: Rising");
-  }else{
-    tft.println("Trigger: Falling");
-  }
+
+    if(!trigger_state){
+      tft.println("Trigger: Rising");
+    }else{
+      tft.println("Trigger: Falling");
+    }
   trigger_function();
   static uint32_t plotTime = micros(); // used to be = millis(), now using micros
-  //float display_data[100];
-  //float real_data[100];
   int Amplitude = (findMax(real_data) - findMin(real_data))/2;
   tft.setCursor(110, 5);
   tft.fillRect(110, 5, 10, 10, TFT_BLACK);
   tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
   tft.println(Amplitude);
-  int y_zoom = 4095;//analogRead(36) + 1; // GPIO 36
+  int y_zoom = analogRead(34) + 1; // GPIO 34
   int x_zoom = analogRead(36) + 1;
   int val = analogRead(35);
   static float gx = 0.0, gy = 0.0;
@@ -249,24 +205,6 @@ void loop() {
   tft.drawNumber((((-50)*y_zoom)/4095), gr.getPointX(0.0), gr.getPointY(-50.0));
   tft.drawNumber(0, gr.getPointX(0.0), gr.getPointY(0.0));
   tft.drawNumber((((50)*y_zoom)/4095), gr.getPointX(0.0), gr.getPointY(50.0));
-
-  //int Zoom = analogRead(36); // GPIO 36
-  // x scale units is from 0 to 100, y scale units is -50 to 50
-  //gr.setGraphScale(0.0, (Zoom/40950), -50.0, 50.0);
-
-  // X grid starts at 0 with lines every 10 x-scale units
-  // Y grid starts at -50 with lines every 25 y-scale units
-  // blue grid
-  //gr.setGraphGrid(0.0, (Zoom/409500), -50.0, 10.0, TFT_BLUE);
-
-  //tft.setTextDatum(TC_DATUM); // Top centre text datum
-  //tft.drawNumber(0, gr.getPointX(0.0), gr.getPointY(-50.0) + 3);
-  //tft.drawNumber((Zoom/(2*40950)), gr.getPointX((Zoom/(2*40950))), gr.getPointY(-50.0) + 3);
-  //tft.drawNumber((Zoom/40950), gr.getPointX((Zoom/40950)), gr.getPointY(-50.0) + 3);
-  //gr.drawGraph(20, 20);
-  // Sample periodically
-  //rising edge trigger
-  
   
   if (micros() - plotTime >= SampleTime) { //edited from sampletime
     plotTime = micros();
@@ -292,154 +230,13 @@ void loop() {
 
       // Draw empty graph at 40,10 on display
       gr.drawGraph(20, 20);
-      //for (int i=1; i <100; i++){
-        //tft.drawLine(i-1, data_buffer[i-1], i, data_buffer[i], TFT_BLACK);
-      //}
-      
-      //tr2.startTrace(TFT_BLACK); Store values at each point and plot each point with a black spot to erase instead of drawing graph again
+    
       // Start new trace
       //tr1.startTrace(TFT_GREEN);
       tr2.startTrace(TFT_YELLOW);
     }
     
   }
-  
-  // put your main code here, to run repeatedly:
-  //Display code:
-  //-------Display demo code-------
-  //for(uint8_t rotation=0; rotation<4; rotation++) {
-    //tft.setRotation(rotation);
-    //testText();
-    //testFillScreen();
-    //testLines(ILI9341_GREEN);
-    //delay(500); // 1000 is 1 sec
-  //}
-  //--------------------------------
-
-  // +++++++++++  This Code below was for Block 1 and 2 Verification. Contents include:
-  // +++++++++++  Push button and Potentiometer response
-  // +++++++++++  ESPTimer code implementation
-  // +++++++++++  Accepting input from the serial monitor.
-  // +++++++++++  Pin assignments are oudated.
-  /*
-  if (Serial.available() >0){
-    receivedChar = Serial.read();
-  }
-
-  if (receivedChar == 't'){
-    //ceivedChar = 'x';
-    const esp_timer_create_args_t small_timer_args={
-    .callback = small_timer_callback, .name = "small"};
-
-    esp_timer_handle_t small_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&small_timer_args, &small_timer));
-    // The timer has been created but is not running yet  
-
-    const esp_timer_create_args_t big_timer_args={
-    .callback = big_timer_callback, .arg = (void*) small_timer , .name = "big"};
-
-    esp_timer_handle_t big_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&big_timer_args, &big_timer));
-    // Start the timers 
-    Serial.println();
-    ESP_ERROR_CHECK(esp_timer_start_periodic(small_timer, 500000));// 500000 -> 0.5s, 0.01s ->10000, 0.000001s -> 1
-    ESP_ERROR_CHECK(esp_timer_start_once(big_timer, 5000000)); // 5000000 -> 5s, 1s-> 1000000, 50us->50
-    long int x = esp_timer_get_time();
-    ///Serial.print("time since boot: ");
-    //Serial.print(x);
-    //ESP_LOGI("Started timers, time since boot: %lld us", esp_timer_get_time(););
-
-    // Print debugging information about timers to console every 2 seconds 
-    for (int i = 0; i < 5; ++i) {
-        ESP_ERROR_CHECK(esp_timer_dump(stdout));
-        usleep(2000000);
-    }
-  // Clean up and finish the example 
-    ESP_ERROR_CHECK(esp_timer_stop(small_timer));
-    ESP_ERROR_CHECK(esp_timer_delete(small_timer));
-    ESP_ERROR_CHECK(esp_timer_delete(big_timer));
-    //Serial.println("Stopped and deleted timers");
-    //ESP_LOGI(TAG, "Stopped and deleted timers");
-    receivedChar = 'x';
-    
-  } else if (receivedChar == 'c'){
-    int example_in =volts_to_bits(2.5);
-    double example_out = bits_to_volts(example_in);
-    
-    Serial.println(volts_to_bits(3));
-    Serial.println(bits_to_volts(0b11111111111111));
-    receivedChar = 'x';
-
-  } else if (receivedChar == 'b'){
-    const esp_timer_create_args_t small_timer_args={
-    .callback = small_timer_callback, .name = "small"};
-
-    esp_timer_handle_t small_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&small_timer_args, &small_timer));
-    // The timer has been created but is not running yet 
-
-    const esp_timer_create_args_t big_timer_args={
-    .callback = big_timer_callback, .arg = (void*) small_timer , .name = "big"};
-
-    esp_timer_handle_t big_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&big_timer_args, &big_timer));
-    // Start the timers 
-    Serial.println();
-    //ESP_ERROR_CHECK(esp_timer_start_periodic(small_timer, 500000));// 500000 -> 0.5s, 0.01s ->10000, 0.000001s -> 1
-    //ESP_ERROR_CHECK(esp_timer_start_once(big_timer, 5000000)); // 5000000 -> 5s, 1s-> 1000000, 50us->50
-    //long int x = esp_timer_get_time();
-    //Serial.print("time since boot: ");
-    //Serial.print(x);
-    //ESP_LOGI("Started timers, time since boot: %lld us", esp_timer_get_time(););
-
-    //Print debugging information about timers to console every 2 seconds 
-    //for (int i = 0; i < 5; ++i) {
-        //ESP_ERROR_CHECK(esp_timer_dump(stdout));
-        //usleep(2000000);
-    
-  // Clean up and finish the example 
-    //ESP_ERROR_CHECK(esp_timer_stop(small_timer));
-    ////ESP_ERROR_CHECK(esp_timer_delete(small_timer));
-    //ESP_ERROR_CHECK(esp_timer_delete(big_timer));
-    //Serial.println("Stopped and deleted timers");
-    //ESP_LOGI(TAG, "Stopped and deleted timers");
-      //digitalWrite(33, HIGH);
-      if (digitalRead(button)){
-          Serial.println("Pressed ");
-          Serial.println(esp_timer_get_time());
-      } else {
-        Serial.println("UnPressed");
-        Serial.println(esp_timer_get_time());
-      }
-  }else if (receivedChar == 'p'){
-      int potVal = analogRead(36); // GPIO 36
-      Serial.println(potVal);
-      Serial.println(esp_timer_get_time());
-      Serial.println("");
-
-
-
-  }
-      //digitalWrite(33, HIGH);
-      //if (Ch1){
-        //Serial.println("Ch1");
-        //}else{
-         // Serial.println("Ch2");
-         // }
-
-      //if (digitalRead(button) == 1){
-        //if (Ch1){
-          //Ch1 = 0;
-          //Ch2 = 1;
-          //Serial.println("Ch2");
-        //} else {
-          //Ch2 = 0;
-          //Ch1 = 1;
-          //Serial.println("Ch1");
-        //}
-
-        //+++++++++ End of Block code, not including functions below
-        */
 }
 }    
 
@@ -773,79 +570,6 @@ unsigned long testFilledRoundRects() {
   return micros() - start;
 }
   
-  
-  
-  /*
-   else if (receivedChar == 'g'){
-
-    ImGui::CreateContext();
-    ImPlot::CreateContext();
-
-    ImPlot::DestroyContext();
-    ImGui::DestroyContext();
-
-
-  }*/
-
-
-  
-  
-//}
-
-// put function definitions here:
-/*
-static void small_timer_callback(void* arg)
-{
-    int64_t time_since_boot = esp_timer_get_time();
-    Serial.println();
-    Serial.print("Small timer called, time since boot: ");
-    Serial.print(time_since_boot);
-    //ESP_LOGI(TAG, "Small timer called, time since boot: %lld us", time_since_boot);
-}*/
-
-
-/*
-static void big_timer_callback(void* arg)
-{
-    int64_t time_since_boot = esp_timer_get_time();
-    Serial.println();
-    Serial.print("Big timer called, time since boot: ");
-    Serial.print(time_since_boot);
-    //ESP_LOGI(TAG, "Big timer called, time since boot: %lld us", time_since_boot);
-    esp_timer_handle_t small_timer_handle = (esp_timer_handle_t) arg;
-    // To start the timer which is running, need to stop it first 
-    ESP_ERROR_CHECK(esp_timer_stop(small_timer_handle));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(small_timer_handle, 1000000));
-    time_since_boot = esp_timer_get_time();
-    Serial.println();
-    Serial.print("Restarted small timer with 1s period, time since boot: ");
-    Serial.print(time_since_boot);
-    //ESP_LOGI(TAG, "Restarted small timer with 1s period, time since boot: %lld us", time_since_boot);
-}
-
-*/
-
-
-
-
-
-/*
-static double bits_to_volts(double bit_in){
-  int positive_x_axis = 8191;
-  int v_zoom = 1;
-  double volts = (bit_in - positive_x_axis)*(5)/(positive_x_axis*v_zoom);
-  return volts;
-}
-*/
-
-/*
-static double volts_to_bits(double volt_in){
-  int positive_x_axis = 8191;
-  int v_zoom = 1;
-  double bits = positive_x_axis + (volt_in*positive_x_axis*v_zoom)/5;
-  return round(bits);
-}
-  */
 
   /*
 int ADC_val_converter(int example_input){
@@ -862,11 +586,3 @@ int ADC_val_converter(int example_input){
 }
 */
 
-/* esp_timer (high resolution timer) example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
